51a3ba42ca9106c88eb13dc3a2f7d269a96fd122
Adan <adanramadhan2001@gmail.com>
2025-02-10 20:58:44 +0700

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { lazy} from "react";
+import { Analytics } from "@vercel/analytics/react"
 
 const Home = lazy(() => import("./pages/Homepage"));
 const Project = lazy(() => import("./pages/project"));
@@ -8,6 +9,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Analytics/>
           <Route path="/" element={<Home />} />
           <Route path="/project" element={<Project />} />
       </Routes>
